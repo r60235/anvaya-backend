6e1234337fe88f61c4afbb312aad880771540a17
admin <admin-email@example.com>
2025-11-22 13:58:45 +0530

    additional configs & dependencies added

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const cors = require("cors");
-const connectDB = require('./db/connection.db');
+// const express = require('express');
+// const cors = require("cors");
+// const connectDB = require('./db/connection.db');
 
-const app = express();
-app.use(express.json());
+// const app = express();
+// app.use(express.json());
 
-const corsOptions = {
-  origin: "*",
-  credentials: true,
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: "*",
+//   credentials: true,
+//   optionSuccessStatus: 200,
+// };
+// app.use(cors(corsOptions));
 
-// connect to DB
-connectDB();
+// // connect to DB
+// connectDB();
